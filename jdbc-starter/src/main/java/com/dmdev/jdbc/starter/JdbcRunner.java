7e3848b78096f54aa795aab42a0ed1172bf5e77d
YurySkosyrev <skosyrev-87@mail.ru>
2023-04-25 23:47:28 +0300

    Step 1.7/4 part 1

--- jdbc-starter/src/main/java/com/dmdev/jdbc/starter/JdbcRunner.java
+++ jdbc-starter/src/main/java/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -3,6 +3,7 @@ package com.dmdev.jdbc.starter;
 import com.dmdev.jdbc.starter.util.ConnectionManager;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,13 +17,41 @@ public class JdbcRunner {
             );
             """;
 
+        String insertSql = """
+            INSERT INTO info (date)
+            VALUES 
+            ('Test1'),
+            ('Test2'),
+            ('Test3'),
+            ('Test4')
+            ;
+            """;
+
+        String updateSql = """
+                UPDATE info
+                SET date = 'TestTest'
+                WHERE id = 5
+                RETURNING *
+                """;
+
+        String selectSql = """
+                SELECT *
+                FROM ticket;
+                """;
 
         try (Connection connection = ConnectionManager.open();
              Statement statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
 
-            boolean executeResult = statement.execute(sql);
-            System.out.println(executeResult);
+            ResultSet executeResult = statement.executeQuery(selectSql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id") + " " +
+                        executeResult.getString("passenger_no"));
+
+            }
+//            System.out.println(executeResult);
+//            System.out.println(statement.getUpdateCount());
         }
 
     }
